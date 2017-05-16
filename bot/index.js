18c4346564611f14160274a5bfca5098851afe47
reuven <ereuven@gmail.com>
2017-05-16 17:09:39 +0300

    separate reply handler from server code

--- bot/index.js
+++ bot/index.js
@@ -1,11 +1,17 @@
 'use strict';
-//const http = require('http');
 const https = require('https');
 const fs = require('fs');
 
+// Webhook port (facebook will access to https://myserver.com:4488)
+// Facebook doesn't work with http, only https allowed
 const PORT = 4488;
-const Bot = require('./bot_framework');
 
+// bot class
+const Bot = require('./bot_framework');
+// import reply handler functions
+const repHandler = require('./replyHandler');
+
+// initialize bot
 let bot = new Bot({
     // page token
     token: 'EAATS43ZAMkJQBAJBYqHuDcbO3YezZAtK0ekf82lZAZB4ll1BshhtzkFZCDJu7N0EAT0ciav3MZC9FTeKD1ebiWvceTbxbuuAMGtcuLUNhA5D1JoZCyBvcK65ZCZBBXuZBZBFKbDHzkckq67YVf8hIbnCyZAZASEcddUMbeZB2ZCikLMmtgJ1AZDZD',
@@ -17,39 +23,22 @@ let bot = new Bot({
     app_secret: '1b27655a518d7d7ee75312074afabe09'
 });
 
+// bot error handler
 bot.on('error', (err) => {
     console.log(err.message)
 });
 
+// bot message handler
 bot.on('message', (payload, reply) => {
+    // get message text
     let text = payload.message.text;
 
+    // get profile info
     bot.getProfile(payload.sender.id, (err, profile) => {
         if (err) throw err;
 
-        var rep;
+        var rep = handleReply(text);
 
-        if (text == "testme"){
-            rep = {
-                "text":"Pick a color:",
-                "quick_replies":[
-                    {
-                        "content_type":"text",
-                        "title":"Red",
-                        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-                    },
-                    {
-                        "content_type":"text",
-                        "title":"Green",
-                        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-                    }
-                ]
-            };
-        } else {
-            rep = {text};
-        }
-
-        //reply({text}, (err) => {
         reply(rep, (err) => {
             if (err) throw err;
             console.log(`Echoed back to ${profile.first_name} ${profile.last_name}[id: ${payload.sender.id}]: ${text}`);
@@ -57,7 +46,8 @@ bot.on('message', (payload, reply) => {
     });
 });
 
-//var server = http.createServer(bot.middleware());
+
+// create and start webhook server
 var server = https.createServer({
 	ca: fs.readFileSync('bundle.crt'),
     pfx: fs.readFileSync('zoiSiteServer.pfx'),
