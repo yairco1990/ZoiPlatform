5abcb396f345f54a27e671aa68ebc1206c740d5e
Reuven <ereuven@gmail.com>
2017-05-17 02:51:01 -0400

    working

--- bot/index.js
+++ bot/index.js
@@ -29,11 +29,12 @@ bot.on('error', (err) => {
 
 // bot message handler
 bot.on('message', (payload, reply) => {
-	//console.log(payload);
+	console.log(payload);
 	
 	// get message text
 	// if message has no text (e.g. location response) - stringify it.
     let text = !!payload.message.text ? payload.message.text : JSON.stringify(payload.message);
+	let msg_payload = !!payload.message.quick_reply && !!payload.message.quick_reply.payload ? payload.message.quick_reply.payload : null;
 
     // get profile info
     bot.getProfile(payload.sender.id, (err, profile) => {
@@ -44,7 +45,7 @@ bot.on('message', (payload, reply) => {
         let sender_id = payload.sender.id;
 
         // build reply
-        let rep = repHandler.buildReply(text);
+        let rep = repHandler.buildReply(text, msg_payload);
 
         // send reply
         reply(rep, (err) => {
--- bot/replyHandler.js
+++ bot/replyHandler.js
@@ -1,55 +1,66 @@
-function buildReply(message) {
+function buildReply(message, payload) {
     var result = {};
 
-    if (message.toLowerCase() == "qr_text") {
-        result = {
-            "text": "Pick a color:",
-            "quick_replies": [
-                {
-                    "content_type": "text",
-                    "title": "Red",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-                },
-                {
-                    "content_type": "text",
-                    "title": "Green",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-                }
-            ]
-        };
-    } else if (message.toLowerCase() == "qr_pics") {
-        result = {
-            "text": "Choose your favorite:",
-            "quick_replies": [
-                {
-                    "content_type": "text",
-                    "title": "Justin Bieber",
-                    "payload": "JUSTIN",
-                    "image_url": "http://cdn3.thr.com/sites/default/files/2013/11/9713_01_0270.jpg"
-                },
-                {
-                    "content_type": "text",
-                    "title": "Lili Simmons",
-                    "payload": "LILI",
-                    "image_url": "http://static.cinemagia.ro/img/db/actor/36/58/38/lili-simmons-959934l.jpg"
-                }
-            ]
-        };
-    } else if (message.toLowerCase() == "qr_location") {
-        result = {
-            "text": "Please share your location:",
-            "quick_replies": [
-                {
-                    "content_type": "location",
-                }
-            ]
-        };
-    } else {
-        result = {
-            "text": message,
-        }
-    }
-
+	if (payload){
+		if (payload == "LILI"){
+			result = {
+				"text": "Good choice!",
+			};
+		} else if (payload == "JUSTIN"){
+			result = {
+				"text": "You are gay!",
+			};
+		}
+	} else {	
+		if (message.toLowerCase() == "qr_text") {
+			result = {
+				"text": "Pick a color:",
+				"quick_replies": [
+					{
+						"content_type": "text",
+						"title": "Red",
+						"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+					},
+					{
+						"content_type": "text",
+						"title": "Green",
+						"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+					}
+				]
+			};
+		} else if (message.toLowerCase() == "qr_pics") {
+			result = {
+				"text": "Choose your favorite:",
+				"quick_replies": [
+					{
+						"content_type": "text",
+						"title": "Justin Bieber",
+						"payload": "JUSTIN",
+						"image_url": "http://cdn3.thr.com/sites/default/files/2013/11/9713_01_0270.jpg"
+					},
+					{
+						"content_type": "text",
+						"title": "Lili Simmons",
+						"payload": "LILI",
+						"image_url": "http://static.cinemagia.ro/img/db/actor/36/58/38/lili-simmons-959934l.jpg"
+					}
+				]
+			};
+		} else if (message.toLowerCase() == "qr_location") {
+			result = {
+				"text": "Please share your location:",
+				"quick_replies": [
+					{
+						"content_type": "location",
+					}
+				]
+			};
+		} else {
+			result = {
+				"text": message,
+			};
+		}
+	}
     return result;
 }
 
