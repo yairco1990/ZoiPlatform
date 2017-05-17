0e91394f9cb217ea1bdd9f49129330ffc1e2c2a8
reuven <ereuven@gmail.com>
2017-05-17 08:29:33 +0300

    add pictures and location quick replies

--- bot/replyHandler.js
+++ bot/replyHandler.js
@@ -1,19 +1,46 @@
-function buildReply(message){
+function buildReply(message) {
     var result = {};
 
-    if (message == "testme"){
+    if (message.toLowerCase() == "qr_text") {
         result = {
-            "text":"Pick a color:",
-            "quick_replies":[
+            "text": "Pick a color:",
+            "quick_replies": [
                 {
-                    "content_type":"text",
-                    "title":"Red",
-                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                    "content_type": "text",
+                    "title": "Red",
+                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
                 },
                 {
-                    "content_type":"text",
-                    "title":"Green",
-                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+                    "content_type": "text",
+                    "title": "Green",
+                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+                }
+            ]
+        };
+    } else if (message.toLowerCase() == "qr_pics") {
+        result = {
+            "text": "Pick a color:",
+            "quick_replies": [
+                {
+                    "content_type": "text",
+                    "title": "Red",
+                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED",
+                    "image_url": "http://petersfantastichats.com/img/red.png"
+                },
+                {
+                    "content_type": "text",
+                    "title": "Green",
+                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN",
+                    "image_url": "http://petersfantastichats.com/img/green.png"
+                }
+            ]
+        };
+    } else if (message.toLowerCase() == "qr_location") {
+        result = {
+            "text": "Please share your location:",
+            "quick_replies": [
+                {
+                    "content_type": "location",
                 }
             ]
         };
