9252e1bddab3dd407660b78add945ad27607f5ae
reuven <ereuven@gmail.com>
2017-05-17 09:02:31 +0300

    use good urls for pictures quick response

--- bot/replyHandler.js
+++ bot/replyHandler.js
@@ -19,19 +19,19 @@ function buildReply(message) {
         };
     } else if (message.toLowerCase() == "qr_pics") {
         result = {
-            "text": "Pick a color:",
+            "text": "Choose your favorite:",
             "quick_replies": [
                 {
                     "content_type": "text",
-                    "title": "Red",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED",
-                    "image_url": "http://petersfantastichats.com/img/red.png"
+                    "title": "Justin Bieber",
+                    "payload": "JUSTIN",
+                    "image_url": "http://cdn3.thr.com/sites/default/files/2013/11/9713_01_0270.jpg"
                 },
                 {
                     "content_type": "text",
-                    "title": "Green",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN",
-                    "image_url": "http://petersfantastichats.com/img/green.png"
+                    "title": "Lili Simmons",
+                    "payload": "LILI",
+                    "image_url": "http://static.cinemagia.ro/img/db/actor/36/58/38/lili-simmons-959934l.jpg"
                 }
             ]
         };
