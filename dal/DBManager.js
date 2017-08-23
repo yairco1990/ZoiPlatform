c1331b079bd8e3449034133f440eb54df584ffd0
Yair <yairco1990@gmail.com>
2017-08-23 17:41:29 +0300

    tested version for v1.0.1 on DEV server

--- dal/DBManager.js
+++ dal/DBManager.js
@@ -10,8 +10,6 @@ class DBManager {
 	constructor() {
 
 		const options = {
-			server: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}},
-			replset: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}},
 			useMongoClient: true
 		};
 
